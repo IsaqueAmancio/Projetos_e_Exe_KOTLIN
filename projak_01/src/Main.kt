0005f0c24efdcd42bfb8e42760978277c335457f
Isaque <isaquealeixo@hotmail.com>
2024-02-24 23:28:40 -0300

    v1

---
+++ projak_01/src/Main.kt
@@ -0,0 +1,11 @@
+import java.util.Scanner
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var mano = 4
+    var mano2 = 2
+    println("${mano2 + mano}")
+    val nome = readLine() ?: ""
+    println("${nome}")
+}
